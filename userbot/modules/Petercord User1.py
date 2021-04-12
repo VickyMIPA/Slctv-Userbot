84bb0efb10a9b05c1c61cebcfb259c542d115821
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-12 14:02:46 +0700

    Update Petercord User1.py

--- userbot/modules/Petercord User1.py
+++ userbot/modules/Petercord User1.py
@@ -302,7 +302,7 @@ async def typewriter(typew):
 
 
 CMD_HELP.update({
-    "vip":
+    "PETERCORD VIP":
     "`.bulan` ; `.hati` ; `.bernyanyi`\
     \nUsage: liat aja.\
     \n\n`.helikopter` ; `.tank` ; `.tembak`\n`.bundir`\
