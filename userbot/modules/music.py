c1f999823d197265630d3beb8611002e6193ad4e
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-12 13:25:33 +0700

    Update music.py

--- userbot/modules/music.py
+++ userbot/modules/music.py
@@ -66,20 +66,20 @@ async def _(event):
     track = str(artist) + " - " + str(song)
     chat = "@WooMaiBot"
     link = f"/netease {track}"
-    await event.edit("`Searching...`")
+    await event.edit("`🐲MENCARI MOHON TUNGGU🐲...`")
     try:
         async with bot.conversation(chat) as conv:
             await asyncio.sleep(2)
-            await event.edit("`Processing... Please wait`")
+            await event.edit("`Lagi prosess... Mohon sabar pasti ada kok..😁`")
             try:
                 msg = await conv.send_message(link)
                 response = await conv.get_response()
                 respond = await conv.get_response()
                 await bot.send_read_acknowledge(conv.chat_id)
             except YouBlockedUserError:
-                await event.reply("`Please unblock @WooMaiBot and try again`")
+                await event.reply("`MOHON unblock @WooMaiBot and try again`")
                 return
-            await event.edit("`Sending Your Music...`")
+            await event.edit("`MENGIRIM MUSIC KAMU JEBRET SELAMAT MENDENGAR...`")
             await asyncio.sleep(3)
             await bot.send_file(event.chat_id, respond)
         await event.client.delete_messages(
