ef82e247cd8a6b953213f9c9701ba89024cbc037
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-15 15:58:30 +0700

    Create Game.py

---
+++ userbot/modules/Game.py
@@ -0,0 +1,63 @@
+#port by ilham mansiez
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot.events import register
+from userbot import CMD_HELP
+
+chat = "@gamee"
+
+
+@register(outgoing=True, pattern="^.ninjagame ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        text, username = event.pattern_match.group(1).split()
+
+    else:
+        await event.edit("`Masukan Yang Benar Ya Biar Bisa Bisa direspon!!`")
+        return
+
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("play grafit ninja 2")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("play grafit ninja 2")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+
+@register(outgoing=True, pattern="^.racergame ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        text, username = event.pattern_match.group(1).split()
+
+    else:
+        await event.edit("`Masukan Yang Benar Ya Biar Bisa Bisa direspon!!`")
+        return
+
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("Play - F1 Racer")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("Play - F1 Racer")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "Kalkulator": ".ninjagame\
+    \nUntuk Membuat Bot Dari Game, .ninjagame "
+    })
