5becb8b0443cc432925b206036de46d7a53efd98
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-15 22:58:47 +0700

    Create calkulator.py

---
+++ userbot/modules/calkulator.py
@@ -0,0 +1,32 @@
+#port by ilham mansiez
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot.events import register
+from userbot import CMD_HELP
+
+chat = "@CalcuBot"
+
+
+@register(outgoing=True, pattern="^.calkulator ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.pattern_match.group(1):
+        text, username = event.pattern_match.group(1).split()
+
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message("/start")
+            audio = await conv.get_response()
+            await conv.send_message("text")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete(conv.chat_id, [msg.id, response.id])
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("/start")
+            audio = await conv.get_response()
+            await conv.send_message("text")
+            audio = await conv.get_response()
+            await event.client.forward_messages(event.chat_id, audio)
+            await event.delete(conv.chat_id, [msg.id, response.id])
