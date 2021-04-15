8e1b8f3be868dd92f1d8cc74dba2e0f09ff5395c
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-15 18:54:13 +0700

    Update petercordfun.py

--- userbot/modules/petercordfun.py
+++ userbot/modules/petercordfun.py
@@ -19,6 +19,17 @@ async def _(event):
 
 # ILham Mansiezz
 
+@register(outgoing=True, pattern=r"^\.game(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@inlinegamesbot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
 
 @register(outgoing=True, pattern=r"^\.wp(?: |$)(.*)")
 async def _(event):
