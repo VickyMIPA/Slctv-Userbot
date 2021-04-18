dd57df27199912ebde4dc748ca89503e29b036cd
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-18 20:18:53 +0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,27 +1,81 @@
-#!/usr/bin/env python3
-# (c) https://t.me/TelethonChat/37677 and SpEcHiDe
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
+export LANG=C.UTF-8
 
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
+echo -e "\nChecking dependencies...\n"
 
-print("""Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details
-Check your Telegram saved messages section to copy the STRING_SESSION""")
-API_KEY = int(input("Enter API_KEY here: "))
-API_HASH = input("Enter API_HASH here: ")
+if command -v python3 >/dev/null 2>&1 ; then
+    echo -e "python3 found "
+    echo -e "version: $(python3 -V)"
+else
+    echo -e "python not found "
+    if [ "$(command -v pkg)" != "" ]; then
+        arr+=(python ) #termux python3 is in python
+    else
+        arr+=(python3 )
+    fi
+fi
 
-with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
-    session_string = client.session.save()
-    saved_messages_template = """Grup Support @petercord
+sleep 1
 
-<code>STRING_SESSION</code>: <code>{}</code>
+if command -v wget >/dev/null 2>&1 ; then
+    echo -e "\nwget found\n"
+else
+    echo -e "\nwget not found\n"
+    arr+=(wget )
+fi
 
-⚠️ <i>Please be careful before passing this value to third parties</i>""".format(session_string)
-    client.send_message("me", saved_messages_template, parse_mode="html")
+DEPENDENCIES=${arr[@]}
+sleep 1
+
+if [ ! -z "$DEPENDENCIES" ]; then
+    echo -e "\nInstalling required dependencies\n"
+    sleep 1
+
+    if [ "$(command -v pkg)" != "" ]; then # termux
+        pkg install "$DEPENDENCIES" -y
+
+    elif [ "$(command -v apt-get)" != "" ]; then # debian
+        sudo apt-get install "$DEPENDENCIES" -y
+
+    elif [ "$(command -v pacman)" != "" ]; then # arch
+        sudo pacman -S "$DEPENDENCIES" -y
+
+# Free to PR to add others
+    else
+        echo -e "\nDistro not supported \nInstall this packages yourself: $DEPENDENCIES\n"
+    fi
+
+else
+    echo -e "\nDependencies have been installed. \nContinuing to install python packages(PyPI)\n"
+    sleep 1
+fi
+
+echo -e "\nUpgrading python pip\n"
+pip3 install --upgrade pip setuptools
+echo -e "\nInstalling telethon...\n"
+pip3 install telethon
+sleep 2
+
+if [ ! -e string_session.py ]; then
+    echo -e "\nDownloading string_session.py\n"
+    wget https://raw.githubusercontent.com/Zora24/Lord-Userbot/Lord-Userbot/string_session.py
+
+    echo -e "\nRunning script...\n"
+    sleep 1
+    python3 string_session.py
+else
+    echo -e "\nstring_session.py detected... \nrunning file\n"
+    sleep 1
+    python3 string_session.py
+fi
+
+echo -e "Do you want to cleanup your file?"
+echo -e "[1] cleanup: this delete string_session.py and this file"
+echo -e "[2] exit"
+echo -ne "\nEnter your choice[1-2]: "
+read choice
+if [ "$choice" = "1" ]; then
+    echo -e "Cleanup: removing file"
+    rm -f string_session.py Getstring.sh
+elif [ "$choice" = "2" ]; then
+    exit
+fi
