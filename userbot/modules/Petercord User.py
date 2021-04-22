6c3f700f49856c25acdcb11b3758a2ce75d88bf7
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-23 00:59:22 +0700

    Update Petercord User.py

--- userbot/modules/Petercord User.py
+++ userbot/modules/Petercord User.py
@@ -728,7 +728,7 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern='^.botgambar(?: |$)(.*)')
+@register(outgoing=True, pattern='^.api(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await pong.edit(" 
