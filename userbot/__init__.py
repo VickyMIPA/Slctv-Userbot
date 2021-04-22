44b829847f5e4c71d75bb366a3662b5289a9d986
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-04-23 01:25:58 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -367,9 +367,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                 custom.Button.inline(
                     "🗡", data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                custom.Button.inline(
-                    "❎", data="{}_back({})".format(prefix, modulo_page)
-                ),
+                custom.Button.inline("Close", data="close")
                 custom.Button.inline(
                     "🗡", data="{}_next({})".format(prefix, modulo_page)
                 )
