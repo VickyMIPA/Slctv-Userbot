2531b71bce10d0d5f4cceb6195fb2f686b162739
Mr.Miss <keselekpermen69@gmail.com>
2021-04-22 17:21:55 +0000

    pylint: auto fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,9 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 🎸".format("🎸", x), data="ub_modul_{}".format(x))
+        custom.Button.inline(
+    "{} {} 🎸".format(
+        "🎸", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
