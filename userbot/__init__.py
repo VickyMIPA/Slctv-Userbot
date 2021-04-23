2b95235d8f0d6fee73c4a81302b37068a5aab68b
Mr.Miss <keselekpermen69@gmail.com>
2021-04-23 04:41:44 +0000

    pylint: auto fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,9 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 🎸".format("🎸", x), data="ub_modul_{}".format(x))
+        custom.Button.inline(
+    "{} {} 🎸".format(
+        "🎸", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -382,36 +384,36 @@ with bot:
     try:
         tgbot = TelegramClient(
             "TG_BOT_TOKEN",
-            api_id=API_KEY,
-            api_hash=API_HASH).start(
-            bot_token=BOT_TOKEN)
+            api_id = API_KEY,
+            api_hash = API_HASH).start(
+            bot_token = BOT_TOKEN)
 
-        dugmeler = CMD_HELP
-        me = bot.get_me()
-        uid = me.id
+        dugmeler=CMD_HELP
+        me=bot.get_me()
+        uid=me.id
 
-        @tgbot.on(events.NewMessage(pattern="/start"))
+        @ tgbot.on(events.NewMessage(pattern="/start"))
         async def handler(event):
             if event.message.from_id != uid:
                 await event.reply("Petercord-Userbot, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/ilham77mansiz/-PETERCORD-.git)")
             else:
                 await event.reply(f"`Hai Petercord {ALIVE_NAME}\n\nApa Kabarmu?`")
 
-        @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+        @ tgbot.on(events.InlineQuery)  # pylint:disable=E0602
         async def inline_handler(event):
-            builder = event.builder
-            result = None
-            query = event.text
+            builder=event.builder
+            result=None
+            query=event.text
             if event.query.user_id == uid and query.startswith("@UserButt"):
-                buttons = paginate_help(0, dugmeler, "helpme")
-                result = builder.article(
+                buttons=paginate_help(0, dugmeler, "helpme")
+                result=builder.article(
                     "Harap Gunakan .help Untuk Perintah",
-                    text="{}\n\n**🔰 Jumlah Modul Yang Tersedia:** `{}`\n               \n**🔰 Daftar Modul  🎸PETERCORD-USERBOT🎸:** \n".format(
+                    text = "{}\n\n**🔰 Jumlah Modul Yang Tersedia:** `{}`\n               \n**🔰 Daftar Modul  🎸PETERCORD-USERBOT🎸:** \n".format(
                         "** 🎸PETERCORD-USERBOT🎸**",
                         len(dugmeler),
                     ),
-                    buttons=buttons,
-                    link_preview=False,
+                    buttons = buttons,
+                    link_preview = False,
                 )
             elif query.startswith("tb_btn"):
                 result = builder.article(
