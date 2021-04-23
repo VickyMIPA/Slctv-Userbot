d2bad9a996c1b5177ed1822cec982308f48f1cfd
Mr.Miss <keselekpermen69@gmail.com>
2021-04-23 02:58:28 +0000

    pylint: auto fixes

--- userbot/modules/button.py
+++ userbot/modules/button.py
@@ -5,7 +5,6 @@ import re
 from telethon import Button
 
 
-
 # regex obtained from:
 # https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
 BTN_URL_REGEX = re.compile(
