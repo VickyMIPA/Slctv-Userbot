6d61494789485cd9dd0c5fcd40a34e48385aa1f3
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-02 04:10:32 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -27,9 +27,9 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t🛡  "
-        await event.edit("**🛡 Daftar Perintah Untuk\n🛡PETERCORD-USERBOT🛡:\n\n**"
-                         f"🛡{string}🛡"
+            string += "`\t🎸  "
+        await event.edit("**🎸 Daftar Perintah Untuk\n🛡PETERCORD-USERBOT🛡:\n\n**"
+                         f"🎸{string}🎸"
                          "\n PETERCORD-USERBOT ,HANYA UNTUK BERSENANG SENANG DI TELEGRAM")
         await event.reply(f"\n**Ketik Contoh** `.help petercordkata-kata` **Untuk Informasi Perintah**")
         await asyncio.sleep(1000)
