419d7ebe6226400dfe5c2867f1aab18bdddda7b6
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-03 10:21:42 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "✅ **Watshapp Userbot:** [[Watshapp-Userbot]](https://github.com/BOT-PROJECT-MANSIEZ/Watsahapa-AsenaMansiez)\n🚹 **Pemilik:** [[🎸ILham Mansiezz🎸]](t.me/diemmmmmmmmmm)"
+        "✅ **Watshapp Userbot:** [[Watshapp-Userbot]](https://github.com/BOT-PROJECT-MANSIEZ/Watsahapa-AsenaMansiez)\n🚹 **Pemilik:** [🎸ILham Mansiezz🎸](t.me/diemmmmmmmmmm)"
     )
 
 
@@ -128,7 +128,7 @@ async def repo_is_here(wannasee):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "🔰 **Repo Userbot:** [Petercord-Userbot](https://github.com/ilham77mansiz/-PETERCORD-)\n🚹 **Pemilik:** [[🎸ILham Mansiezz🎸]](t.me/diemmmmmmmmmm)"
+        "🔰 **Repo Userbot:** [Petercord-Userbot](https://github.com/ilham77mansiz/-PETERCORD-)\n🚹 **Pemilik:** [🎸ILham Mansiezz🎸](t.me/diemmmmmmmmmm)"
     )
 
 
