b64299b653ad1618cf7859d2129901c1cc73dfb3
Mr.Miss <keselekpermen69@gmail.com>
2021-05-08 07:10:06 +0000

    pylint: auto fixes

--- userbot/modules/globalban.py
+++ userbot/modules/globalban.py
@@ -2,7 +2,6 @@
 from telethon.events import ChatAction
 from userbot import ALIVE_NAME, CMD_HELP, bot
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
-from telethon.tl.types import ChatBannedRights
 
 from userbot.events import register
 from telethon.tl.types import MessageEntityMentionName
@@ -120,7 +119,7 @@ async def gban(userbot):
         except BaseException:
             pass
         try:
-            await userbot.client(BlockRequest,(user))
+            await userbot.client(BlockRequest, (user))
         except BaseException:
             pass
         testuserbot = [
