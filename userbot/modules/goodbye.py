f06166dcb0eb783bc7bc04fb8e6119a0f153a52f
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-10 12:19:33 +0700

    Create goodbye.py

---
+++ userbot/modules/goodbye.py
@@ -0,0 +1,164 @@
+#Port ilham mansiz
+from userbot.events import register
+from userbot import CMD_HELP, bot, LOGS, CLEAN_WELCOME, BOTLOG_CHATID
+from telethon.events import ChatAction
+
+@bot.on(ChatAction)
+async def goodbye_to_chat(event):
+    try:
+        from userbot.modules.sql_helper.goodbye_sql import get_current_goodbye_settings
+        from userbot.modules.sql_helper.goodbye_sql import update_previous_goodbye
+    except:
+        return
+    cws = get_current_goodbye_settings(event.chat_id)
+    if cws:
+        """user_added=False,
+        user_joined=False,
+        user_left=True,
+        user_kicked=True"""
+        if (event.user_left
+                or event.user_kicked) and not (await event.get_user()).bot:
+            if CLEAN_WELCOME:
+                try:
+                    await event.client.delete_messages(event.chat_id,
+                                                       cws.previous_goodbye)
+                except Exception as e:
+                    LOGS.warn(str(e))
+            a_user = await event.get_user()
+            chat = await event.get_chat()
+            me = await event.client.get_me()
+
+            title = chat.title if chat.title else "this chat"
+            participants = await event.client.get_participants(chat)
+            count = len(participants)
+            mention = "[{}](tg://user?id={})".format(a_user.first_name,
+                                                     a_user.id)
+            my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
+            first = a_user.first_name
+            last = a_user.last_name
+            if last:
+                fullname = f"{first} {last}"
+            else:
+                fullname = first
+            username = f"@{a_user.username}" if a_user.username else mention
+            userid = a_user.id
+            my_first = me.first_name
+            my_last = me.last_name
+            if my_last:
+                my_fullname = f"{my_first} {my_last}"
+            else:
+                my_fullname = my_first
+            my_username = f"@{me.username}" if me.username else my_mention
+            file_media = None
+            current_saved_goodbye_message = None
+            if cws and cws.f_mesg_id:
+                msg_o = await event.client.get_messages(entity=BOTLOG_CHATID,
+                                                        ids=int(cws.f_mesg_id))
+                file_media = msg_o.media
+                current_saved_goodbye_message = msg_o.message
+            elif cws and cws.reply:
+                current_saved_goodbye_message = cws.reply
+            current_message = await event.reply(
+                current_saved_goodbye_message.format(mention=mention,
+                                                     title=title,
+                                                     count=count,
+                                                     first=first,
+                                                     last=last,
+                                                     fullname=fullname,
+                                                     username=username,
+                                                     userid=userid,
+                                                     my_first=my_first,
+                                                     my_last=my_last,
+                                                     my_fullname=my_fullname,
+                                                     my_username=my_username,
+                                                     my_mention=my_mention),
+                file=file_media)
+            update_previous_goodbye(event.chat_id, current_message.id)
+
+
+@register(outgoing=True, pattern=r"^.setgoodbye(?: |$)(.*)")
+async def save_goodbye(event):
+    try:
+        from userbot.modules.sql_helper.goodbye_sql import add_goodbye_setting
+    except:
+        await event.edit("`Berjalan Pada Mode Non-SQL!`")
+        return
+    msg = await event.get_reply_message()
+    string = event.pattern_match.group(1)
+    msg_id = None
+    if msg and msg.media and not string:
+        if BOTLOG_CHATID:
+            await event.client.send_message(
+                BOTLOG_CHATID, f"📢GOODBYE PETERCORD📢\
+            \nGRUP ID: {event.chat_id}\
+            \nPetercord Memasang Pesan Perintah Welcome Digrup, Ini Adalah Catatan Pesan Welcome "
+             "Mohon Jangan Dihapus 🎸Petercord!🎸"
+            msg_o = await event.client.forward_messages(
+                entity=BOTLOG_CHATID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True)
+            msg_id = msg_o.id
+        else:
+            await event.edit(
+                "`Untuk membuat media sebagai pesan GOODBYE, BOTLOG_CHATID Harus disetel.`"
+            )
+            return
+    elif event.reply_to_msg_id and not string:
+        rep_msg = await event.get_reply_message()
+        string = rep_msg.text
+    success = "`Berhasil Menyimpan Pesan GOODBYE {} ツ`"
+    if add_goodbye_setting(event.chat_id, 0, string, msg_id) is True:
+        await event.edit(success.format('Disini'))
+    else:
+        await event.edit(success.format('Disini'))
+
+
+@register(outgoing=True, pattern="^.checkgoodbye$")
+async def show_goodbye(event):
+    try:
+        from userbot.modules.sql_helper.goodbye_sql import get_current_goodbye_settings
+    except:
+        await event.edit("`Running on Non-SQL mode!`")
+        return
+    cws = get_current_goodbye_settings(event.chat_id)
+    if not cws:
+        await event.edit("`Disini tidak ada pesan goodbye Petercord.`")
+        return
+    elif cws and cws.f_mesg_id:
+        msg_o = await event.client.get_messages(entity=BOTLOG_CHATID,
+                                                ids=int(cws.f_mesg_id))
+        await event.edit(
+            "`ANDA TELAH MEMBUAT GOODBYE DISINI`")
+        await event.reply(msg_o.message, file=msg_o.media)
+    elif cws and cws.reply:
+        await event.edit(
+            "`ANDA TELAH MEMBUAT GOODBYE DISINI`")
+        await event.reply(cws.reply)
+
+
+@register(outgoing=True, pattern="^.rmgoodbye$")
+async def del_goodbye(event):
+    try:
+        from userbot.modules.sql_helper.goodbye_sql import rm_goodbye_setting
+    except:
+        await event.edit("`Running on Non-SQL mode!`")
+        return
+    if rm_goodbye_setting(event.chat_id) is True:
+        await event.edit("`Menghapus Goodbye Berhasil dilakukan.`")
+    else:
+        await event.edit("`Anda tidak menyimpan goodbye disini ?`")
+
+CMD_HELP.update({
+    "goodbye":
+    ">`.setgoodbye` <pesan welcome> atau balas ke pesan ketik `.setwelcome`"
+    "\nUsage: Menyimpan pesan welcome digrup."
+    "\n\nFormat Variabel yang bisa digunakan dipesan welcome:"
+    "\n`{mention}, {title}, {count}, {first}, {last}, {fullname}, "
+    "{userid}, {username}, {my_first}, {my_fullname}, {my_last}, "
+    "{my_mention}, {my_username}`"
+    "\n\n>`.checgoodbye`"
+    "\nUsage: Check pesan welcome yang anda simpan."
+    "\n\n>`.rmgoodbye`"
+    "\nUsage: Menghapus pesan welcome yang anda simpan."
+})
