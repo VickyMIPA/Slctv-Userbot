6f0d3079c5ea1c9155636d198c0d171b049db78b
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-05-12 16:20:51 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -453,7 +453,8 @@ with bot:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
-        async def close(event):
+        async def on_plug_in_callback_query_handler(event):
+            if event.query.user_id == bot.uid:
             await event.edit("MENU CLOSED PETERCORD by.Tentang Aku dan Dia")
         else:
             reply_pop_up_alert = f"Harap Deploy Petercord Userbot Anda Sendiri, Jangan Menggunakan Milik Petercord {ALIVE_NAME} ツ"
