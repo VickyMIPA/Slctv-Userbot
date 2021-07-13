65acf3d4f2a7d79bfdd5261076f8951d7824c211
VickyMIPA <83190867+VickyMIPA@users.noreply.github.com>
2021-07-14 04:55:01 +0700

    Update Petercord User1.py

--- userbot/modules/Petercord User1.py
+++ userbot/modules/Petercord User1.py
@@ -114,7 +114,7 @@ async def typewriter(typew):
                      "　　　　　 Ｕ Ｕ\n")
 
 
-@register(outgoing=True, pattern='^.awkwok(?: |$)(.*)')
+@register(outgoing=True, pattern='^.kabur(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("────😂──────▀▀▀😂\n"
@@ -340,7 +340,7 @@ CMD_HELP.update({
     \nUsage: liat sendiri\
     \n\n`.y`\
     \nUsage: jempol\
-    \n\n`.awkwok`\
+    \n\n`.kabur`\
     \nUsage: ketawa lari.\
     \n\n`.ular` ; `.babi` ; `.ajg`; `.simbol`\
     \nUsage: liat sendiri."
