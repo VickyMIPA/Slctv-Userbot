86195fa7ebf0c8ca04d38af7f3d7f8fb1cc26e1c
VickyMIPA <83190867+VickyMIPA@users.noreply.github.com>
2021-07-14 04:36:43 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -13,48 +13,48 @@ modules = CMD_HELP
 
 
 @register(outgoing=True, pattern="^.perintahmodul(?: |$)(.*)")
-async def help(petercord):
+async def help(slctv):
     """ For .help command,"""
     args = petercord.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
             await petercord.edit(str(CMD_HELP[args]))
         else:
-            await petercord.edit("**Maaf Petercord, Saya Tidak Punya Perintah Itu :)**")
+            await petercord.edit("**Maaf Slctv, Saya Tidak Punya Perintah Itu :)**")
             await asyncio.sleep(200)
             await petercord.delete()
     else:
         await petercord.edit("DAFTAR PERINTAH PETERCORD")
-        await petercord.edit("**✨ BAGIAN 1:**\n"
+        await petercord.edit("**📍 BAGIAN 1:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`gamepetercord` `petercordkata` `petercorddmisc` `petercordoff` `vip` `animasi` `android` `anime` `anti_spambot` `aria` `ascii` \n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 2:**\n"
+                             "**💡 BAGIAN 2:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`blacklist` `carbon` `chat` `mutechat` `covid` `membuat` `deepfry` `emojigames`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 3:**\n"
+                             "**📍 BAGIAN 3:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`eval` `exec` `term` `frog` `federations` `figlet` `filter` `gban` `gcast` `gdrive` `gcommit` `github`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 4:**\n"
+                             "**💡 BAGIAN 4:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`glitch` `gps` `hash` `base64` `hentai` `heroku` `id` `imgmeme` `kekuatan`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 5:**\n"
+                             "**📍 BAGIAN 5:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`lastfm` `locks` `bantuan` `aeshtetic` `petercorddeteksi` `chatperintah` `phreaker` `hazmat`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 6:**\n"
+                             "**💡 BAGIAN 6:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n `botfather` `amongus` `fontteks` `misc` `app` `link instagram` `grab` `clone`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 7:**\n"
+                             "**📍 BAGIAN 7:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`randomprofil` `petercordmusic` `tiny` `tempmail` `tiktok` `wordcloud`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 8:**\n"
+                             "**💡 BAGIAN 8:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`lyrics` `mega` `memes` `memify` `mentions` `purge` `purgeme` `del` `edit`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 9:**\n"
+                             "**📍 BAGIAN 9:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`sd` `random` `sleep` `shutdown` `repo` `readme` `repeat` `restart`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 10:**\n"
+                             "**💡 BAGIAN 10:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`raw` `nekobot` `notes` `petercord` `petercordfun` `pm` `profil` `quotly` `rastick` `resi` `reverse` `salam` `sangmata`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 11:**\n"
+                             "**📍 BAGIAN 11:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`santetonline` `image_search` `currency` `google` `wiki` `ud` `tts` `translate` `youtube` `rip`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 12:**\n"
+                             "**💡 BAGIAN 12:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`removebg` `ocr` `qrcode` `barcode` `paste` `getpaste` `nekobin` `direct` `screenshot` `sed` `snips` `spam` `spotifynow` `ssvideo`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 13:**\n"
+                             "**📍 BAGIAN 13:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`stickers` `stickers2` `sysd` `botver` `pip` `alive` `tag_all` `telegraph` `timedate` `torrent`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 14:**\n"
+                             "**💡 BAGIAN 14:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n `transform` `update` `download` `getid` `waifu` `wallpaper` `weather`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n"
-                             "**✨ BAGIAN 15:**\n"
+                             "**📍 BAGIAN 15:**\n"
                              "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n`webupload` `welcome` `whois` `ping` `sinyal` `xiaomi` `zipfile` `penghapusankk`\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n\n**By.Tentang Aku Dan Dia**\n")
-        await petercord.reply("\n\n**╭┄┅┯┅┄┄┅┯┅┄╮**\n**CARA MEMAKAINYA,** **CONTOH:**\n**KETIK** `.help petercordoff` **UNTUK INFORMASI MODULES**\n**GROUP SUPPORT:** [PETERCORD-USERBOT](@petercord)\n\n╰┄┅┷┅┄┄┅┷┅┄╯**\n")
+        await petercord.reply("\n\n**╭┄┅┯┅┄┄┅┯┅┄╮**\n**CARA MEMAKAINYA,** **CONTOH:**\n**KETIK** `.help petercordoff` **UNTUK INFORMASI MODULES**\n**GROUP SUPPORT:** [SLCTV-USERBOT](@aliansinomercy)\n\n╰┄┅┷┅┄┄┅┷┅┄╯**\n")
         await asyncio.sleep(1000)
         await petercord.delete()
