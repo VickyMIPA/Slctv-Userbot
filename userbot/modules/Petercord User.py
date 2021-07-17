ce54c65fbc85e6247afe1d8181643bc2c0f94a51
VickyMIPA <83190867+VickyMIPA@users.noreply.github.com>
2021-07-18 06:03:50 +0700

    Update Petercord User.py

--- userbot/modules/Petercord User.py
+++ userbot/modules/Petercord User.py
@@ -787,6 +787,49 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
+@register(outgoing=True, pattern='^.nomercy(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Fajar Peler☑️**")
+    await typew.edit("**Fajar Peler✅**")
+    sleep(1)
+    await typew.edit("**Tebe Gilaa☑️**")
+    await typew.edit("**Tebe Gilaa✅**")
+    sleep(2)
+    await typew.edit("**Popoy Depresi☑️**")
+    await typew.edit("**Popoy Depresi✅**")
+    sleep(2)
+    await typew.edit("**Timeh Gajelas☑️**")
+    await typew.edit("**Timeh Gajelas✅**")
+    sleep(2)
+    await typew.edit("**Aca GJM!☑️**")
+    await typew.edit("**Aca GJM!✅**")
+    sleep(2)
+    await typew.edit("**Dare GJB!☑️**")
+    await typew.edit("**Dare GJB!✅**")
+    sleep(2)
+    await typew.edit("**Vio,MengRibet☑️**")
+    await typew.edit("**Vio,MengRibet✅**")
+    sleep(2)
+    await typew.edit("**Jeje,Mengintil☑️**")
+    await typew.edit("**Jeje,Mengintil✅**")
+    sleep(2)
+    await typew.edit("**Sins,Sijelek☑️**")
+    await typew.edit("**Sins,Sijelek✅**")
+    sleep(2)
+    await typew.edit("**Lexi,Sidongo☑️**")
+    await typew.edit("**Lexi,Sidongo✅**")
+    sleep(2)
+    await typew.edit("**Ulum,Sikontol☑️**")
+    await typew.edit("**Ulum,Sikontol✅**")
+    sleep(2)
+    await typew.edit("**Vicky,Kyutt☑️**")
+    await typew.edit("**Vicky,Kyutt✅**")
+    sleep(3)
+    await typew.edit("**CUMA VICKY YANG BENER!😁**")
+# Create by myself @localheart
+
+
 @register(outgoing=True, pattern="^.ngabuburit$")
 async def koc(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
@@ -982,6 +1025,8 @@ CMD_HELP.update({
     \nUsage:teringat mantan.\
     \n\n`.raziamasker.`\
     \nUsage:Razia masker.\
+    \n\n`.nomercy`\
+    \nUsage:Nomercy kebanggan.\
     \n\n`.ngabuburit` `.voff`.\
     \nUsage:Ngabuburit."
 })
